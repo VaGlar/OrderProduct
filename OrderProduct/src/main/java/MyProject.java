39b682878f688e04027761999537516bf9fcb740
vasil <vasilisglaros@gmail.com>
2023-03-10 15:45:11 +0200

    inserting MyProject (Main)

---
+++ OrderProduct/src/main/java/MyProject.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProject {
+    public static void Main(String[] args){
+        SpringApplication.run(MyProject.class, args);
+    }
+}
